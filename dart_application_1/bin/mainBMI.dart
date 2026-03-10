a5e70eb41e6dfbe0822d775833e613a93b79759d
AhnafRifqi <ahnaf.rifqi1412@gmail.com>
2026-03-11 06:29:06 +0700

    add input validation

--- dart_application_1/bin/mainBMI.dart
+++ dart_application_1/bin/mainBMI.dart
@@ -1,3 +1,12 @@
 void main() {
   List<Map<String, dynamic>> riwayat = [];
 }
+
+bool validasiInput(double tinggiCm, double beratKg) {
+  if (tinggiCm <= 0 || beratKg <= 0) {
+    print("Input tidak valid");
+    return false;
+  }
+  return true;
+}
+
