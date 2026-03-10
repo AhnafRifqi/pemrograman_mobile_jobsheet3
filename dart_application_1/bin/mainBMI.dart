cc4b8e7738cfe94e2cf15e43408b9486af5c7407
AhnafRifqi <ahnaf.rifqi1412@gmail.com>
2026-03-11 06:49:05 +0700

    reset

--- dart_application_1/bin/mainBMI.dart
+++ dart_application_1/bin/mainBMI.dart
@@ -1,35 +1,9 @@
 void main() {
   List<Map<String, dynamic>> riwayat = [];
-
-  prosesBMI(170, 65, riwayat);
-  prosesBMI(165, 75, riwayat);
-  prosesBMI(180, 85, riwayat);
-
-  tampilkanRiwayat(riwayat);
 }
 
-void prosesBMI(double tinggiCm, double beratKg, List<Map<String, dynamic>> riwayat) {
-  if (!validasiInput(tinggiCm, beratKg)) return;
-
-  double bmi = hitungNilaiBMI(tinggiCm, beratKg);
-
-  String kategori;
-
-  if (bmi < 18.5) {
-    kategori = "Kurus";
-  } else if (bmi < 25) {
-    kategori = "Normal";
-  } else if (bmi < 30) {
-    kategori = "Gemuk";
-  } else {
-    kategori = "Obesitas";
-  }
-
-  riwayat.add({
-    'tinggi': tinggiCm,
-    'berat': beratKg,
-    'bmi': bmi,
-    'kategori': kategori
-  });
+double hitungNilaiBMI(double tinggiCm, double beratKg) {
+  double tinggiM = tinggiCm / 100;
+  return beratKg / (tinggiM * tinggiM);
 }
 
