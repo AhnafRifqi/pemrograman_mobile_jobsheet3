898baa01ebde56ec9d5826d2fd30f5a89ef787ad
AhnafRifqi <ahnaf.rifqi1412@gmail.com>
2026-03-11 06:55:42 +0700

    input validation

--- dart_application_1/bin/mainBMI.dart
+++ dart_application_1/bin/mainBMI.dart
@@ -6,3 +6,12 @@ double hitungNilaiBMI(double tinggiCm, double beratKg) {
   double tinggiM = tinggiCm / 100;
   return beratKg / (tinggiM * tinggiM);
 }
+
+bool validasiInput(double tinggiCm, double beratKg) {
+  if (tinggiCm <= 0 || beratKg <= 0) {
+    print("Input tidak valid");
+    return false;
+  }
+  return true;
+}
+
