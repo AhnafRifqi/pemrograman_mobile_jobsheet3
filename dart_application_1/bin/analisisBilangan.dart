59f685ea72d99b81912fc5d5d9a8992c092271b3
AhnafRifqi <ahnaf.rifqi1412@gmail.com>
2026-03-11 07:15:28 +0700

    add user input feature

--- dart_application_1/bin/analisisBilangan.dart
+++ dart_application_1/bin/analisisBilangan.dart
@@ -1,3 +1,10 @@
+import 'dart:io';
+
 void main() {
   print("Program Analisis Bilangan");
+
+  print("Masukkan sebuah angka:");
+  int angka = int.parse(stdin.readLineSync()!);
+
+  print("Angka yang dimasukkan: $angka");
 }
