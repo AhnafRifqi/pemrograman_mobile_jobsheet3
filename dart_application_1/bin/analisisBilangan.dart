3d17560536fbd50952eed9adb3ec8791590d65d4
AhnafRifqi <ahnaf.rifqi1412@gmail.com>
2026-03-11 07:16:34 +0700

    add even and odd number

--- dart_application_1/bin/analisisBilangan.dart
+++ dart_application_1/bin/analisisBilangan.dart
@@ -1,3 +1,14 @@
+import 'dart:io';
+
 void main() {
   print("Program Analisis Bilangan");
+
+  print("Masukkan sebuah angka:");
+  int angka = int.parse(stdin.readLineSync()!);
+
+  if (angka % 2 == 0) {
+    print("Bilangan Genap");
+  } else {
+    print("Bilangan Ganjil");
+  }
 }
