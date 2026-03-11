9312a1891d29197e54e5c3177acabefd299da1bc
AhnafRifqi <ahnaf.rifqi1412@gmail.com>
2026-03-11 07:21:22 +0700

    Add history feature with loop

--- dart_application_1/bin/analisisBilangan.dart
+++ dart_application_1/bin/analisisBilangan.dart
@@ -1,3 +1,41 @@
+import 'dart:io';
+
 void main() {
+  List<Map<String, dynamic>> riwayat = [];
+
   print("Program Analisis Bilangan");
+
+  for (int i = 0; i < 3; i++) {
+    print("Masukkan angka ke-${i + 1}: ");
+    int angka = int.parse(stdin.readLineSync()!);
+
+    String jenis;
+    if (angka % 2 == 0) {
+      jenis = "Genap";
+    } else {
+      jenis = "Ganjil";
+    }
+
+    String sifat;
+    if (angka >= 0) {
+      sifat = "Positif";
+    } else {
+      sifat = "Negatif";
+    }
+
+    riwayat.add({
+      'angka': angka,
+      'jenis': jenis,
+      'sifat': sifat
+    });
+  }
+
+  print("\nRiwayat Analisis:");
+
+  for (var data in riwayat) {
+    print("Angka : ${data['angka']}");
+    print("Jenis : ${data['jenis']}");
+    print("Sifat : ${data['sifat']}");
+    print("-------------------");
+  }
 }
